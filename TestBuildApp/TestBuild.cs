a29983d3311ccedf3af7810f3371a57b8970d3b3
DeepakM-HnH <deepak.mangal@harbourandhills.com>
2024-01-05 12:31:40 +0530

    Add new Visual Studio project

---
+++ TestBuildApp/TestBuild.cs
@@ -0,0 +1,16 @@
+﻿namespace TestBuildApp
+{
+    public class TestBuild
+    {
+        public static void Main()
+        {
+            DisplayMessage();
+        }
+
+        public static void DisplayMessage()
+        {
+            Console.WriteLine("Hello, this is a test build!");
+           
+        }
+    }
+}
